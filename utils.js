066f1041c00107861e3df7aacaeee05dd69ae94c
Mictlantecuhtli-code <boss.fer16@hotmail.com>
2025-09-04 08:39:04 -0600

    Update utils.js

--- utils.js
+++ utils.js
@@ -13,6 +13,13 @@ const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBh
 const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
 
 /**
+ * Función para obtener el cliente Supabase
+ */
+function createClient() {
+    return supabase;
+}
+
+/**
  * SISTEMA DE NOTIFICACIONES
  */
 
