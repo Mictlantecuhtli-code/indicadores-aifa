1434b6e67565e2a7c76cbbe21bde223f5cd4e256
Mictlantecuhtli-code <boss.fer16@hotmail.com>
2025-09-30 15:35:57 -0600

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,9 @@ export default {
         'aifa-light': '#3B82F6',
         'aifa-green': '#047857',
         'aifa-amber': '#F59E0B'
+      },
+      fontFamily: {
+        sans: ['"Nunito"', '"Inter"', 'system-ui', '-apple-system', 'BlinkMacSystemFont', '"Segoe UI"', 'sans-serif']
       }
     }
   },
